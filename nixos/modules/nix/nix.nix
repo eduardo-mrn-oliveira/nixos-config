741a12f7f19a186eb1fc2a96133c0480f2824963
Eduardo Oliveira <eduardo.mrn.oliveira@outlook.com>
2026-07-18 20:36:08 -0300

    Pin Nix outputs and derivations to prevent garbage collection

--- nixos/modules/nix/nix.nix
+++ nixos/modules/nix/nix.nix
@@ -22,4 +22,7 @@
 		substituters = ["https://hyprland.cachix.org"];
 		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 	};
+
+	nix.settings.keep-outputs = true;
+	nix.settings.keep-derivations = true;
 }
