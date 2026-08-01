39d619cda7d58eb1770a232c83cc6bd1806ea3ff
Eduardo Oliveira <eduardo.mrn.oliveira@outlook.com>
2026-08-01 14:54:14 -0300

    enable ghostty systemd service

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -1,10 +1,16 @@
-{
+{config, ...}: {
 	programs.ghostty = {
 		enable = true;
+
+		systemd.enable = true;
 
 		settings = {
 			window-padding-x = 16;
 			window-padding-y = 16;
 		};
 	};
+
+	# Manually satisfy the WantedBy=graphical-session.target directive
+	# since the upstream module bypasses Home Manager's systemd evaluator
+	xdg.configFile."systemd/user/graphical-session.target.wants/app-com.mitchellh.ghostty.service".source = "${config.programs.ghostty.package}/share/systemd/user/app-com.mitchellh.ghostty.service";
 }
